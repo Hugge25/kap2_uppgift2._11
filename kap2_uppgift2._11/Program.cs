731446ff03d518dec83641b6cb2ba35b63f25a8b
EmilWallett <nhlpool27@gmail.com>
2022-09-07 12:11:58 +0200

    Add project files.

---
+++ kap2_uppgift2._11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uppgift_2._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin i meter?");
+            string elin = Console.ReadLine();
+            Console.WriteLine("Hur långt hoppade Alma i meter?");
+            string alma = Console.ReadLine();
+
+            int TalElin = int.Parse(elin);
+            int TalAlma = int.Parse(alma);
+            int skillnad = TalElin - TalAlma;
+
+            Console.WriteLine("Elin hopppade " + skillnad + " längre än Alma");
+        }
+    }
+
+}
